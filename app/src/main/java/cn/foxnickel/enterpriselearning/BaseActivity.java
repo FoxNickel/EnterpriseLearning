5078c5cad14012ba048dcf09b553a543b6aace5d
NickelFox <nickelfox@foxmail.com>
2017-07-05 16:23:59 +0800

    更改了用户名等字体大小，优化了权限申请逻辑

--- app/src/main/java/cn/foxnickel/enterpriselearning/BaseActivity.java
+++ app/src/main/java/cn/foxnickel/enterpriselearning/BaseActivity.java
@@ -1,12 +1,16 @@
 package cn.foxnickel.enterpriselearning;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.foxnickel.enterpriselearning.config.Config;
 
@@ -17,38 +21,38 @@ import cn.foxnickel.enterpriselearning.config.Config;
 
 public class BaseActivity extends AppCompatActivity {
 
+    private final String TAG = getClass().getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!hasPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_SMS)) {
-            requestPermissions(Config.WRITE_EXTERNAL_CODE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_SMS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermission(Config.WRITE_EXTERNAL_CODE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_SMS);
         }
 
     }
 
-    /**
-     * 检查权限
-     *
-     * @param permissions
-     * @return
-     */
-    
-    public boolean hasPermissions(String... permissions) {
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestPermission(int code, String... permissions) {
+        List<String> permissionList = new ArrayList<>();//要申请的权限列表
+        /**
+         * 判断权限是否已经拥有，若未拥有则添加到List中
+         */
         for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false;
+            switch (checkSelfPermission(permission)) {
+                case PackageManager.PERMISSION_GRANTED:
+                    Log.i(TAG, "onCreate: Granted...");
+                    break;
+                case PackageManager.PERMISSION_DENIED:
+                    Log.i(TAG, "onCreate: Denied...");
+                    permissionList.add(permission);
+                    break;
             }
         }
-        return true;
-    }
-
-    /**
-     * 申请权限
-     *
-     * @param permissions
-     */
-    public void requestPermissions(int code, String... permissions) {
-        ActivityCompat.requestPermissions(this, permissions, code);
+        //申请未拥有的权限
+        if (!permissionList.isEmpty()) {
+            requestPermissions(permissionList.toArray(new String[permissionList.size()]), code);
+        }
     }
 
     @Override
